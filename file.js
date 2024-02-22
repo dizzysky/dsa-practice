5b7363eb9b19723ee0f6f705299f5c1ca33300bd
Sanjid Dewan <dsanjid@gmail.com>
2024-02-22 10:15:54 -0500

    bubble sort

---
+++ file.js
@@ -0,0 +1,16 @@
+function bubbleSort(array) {
+	let length = array.length 
+	
+	for(let i=0; i < length; i++) {
+		for (let j = 0; j < length; j++) {
+			if(array[j] > array[j+1]) {
+				let temp = array = array[j];
+				array[j] = array[j+];
+				array[j+1] = temp;
+}
+}
+console.log('iteration ${i+1};', array) 
+}
+return array 
+}
+console.log(bubbleSor(array)); // [1,2,3,4,6]
