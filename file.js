6c8a86ed3f77ed38be8e9b5fc08b3b14fd275cac
Sanjid Dewan <dsanjid@gmail.com>
2024-02-29 10:42:33 -0500

    quicksort

--- file.js
+++ file.js
@@ -17,4 +17,23 @@ console.log(bubbleSor(array)); // [1,2,3,4,6]
 
 
 function quicksort() {
+const quickSort = (arr) => {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  let pivot = arr[0];
+  let leftArr = [];
+  let rightArr = [];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      leftArr.push(arr[i]);
+    } else {
+      rightArr.push(arr[i]);
+    }
+  }
+
+  return [...quickSort(leftArr), pivot, ...quickSort(rightArr)];
+};
 }
